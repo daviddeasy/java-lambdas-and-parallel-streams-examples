9abf40438054123167d79c404e3f9a865ca39169
daviddeasy <david.deasy@accenture.com>
2018-01-07 21:45:58 +0000

    Simple streams experiments

--- ParallelStreams/src/main/java/de/muellerbruehl/parallelstreams/StreamsDemo.java
+++ ParallelStreams/src/main/java/de/muellerbruehl/parallelstreams/StreamsDemo.java
@@ -5,7 +5,9 @@
  */
 package de.muellerbruehl.parallelstreams;
 
+import java.util.Comparator;
 import java.util.List;
+import java.util.Optional;
 import java.util.function.Supplier;
 import java.util.stream.Collectors;
 import java.util.stream.LongStream;
@@ -18,15 +20,16 @@ public class StreamsDemo {
 
   private static final long[] result = new long[1];
 
+  @SuppressWarnings({"ConstantIfStatement", "ConstantConditions"})
   public static void main(String[] args) {
 
-    showParallelProblem();
-    /*
-    System.out.println("\nstarted creating persons - takes a while...");
-    Persons persons = getPersons();
-    System.out.println("created " + persons.getPersons().size() + " persons.\n");
-    countVendors(persons.getPersons());
-    */
+    if (false) showParallelProblem();
+
+    System.out.println("\nStarted creating persons - takes a while...");
+    List<Person> persons = getPersons().getPersons();
+    System.out.println("Created " + persons.size() + " persons.\n");
+
+    simpleStreamsExperiments(persons);
   }
 
   private static Persons getPersons() {
@@ -81,10 +84,11 @@ public class StreamsDemo {
     return null;
   }
 
+  @SuppressWarnings("unused")
   private static Void parallelReduce() {
     for (int i = 0; i < 10; i++) {
-      long preduce = getRange().parallel().reduce(0, StreamsDemo::orderDependentFunction);
-      System.out.println("parallel reduce: " + preduce);
+      long reduce = getRange().parallel().reduce(0, StreamsDemo::orderDependentFunction);
+      System.out.println("parallel reduce: " + reduce);
     }
     return null;
   }
@@ -98,13 +102,30 @@ public class StreamsDemo {
     return Math.abs((a + c) * c);
   }
 
-  private static void countVendors(List<Person> persons) {
-    invokeMethod("Vendors by counting list", () -> getVendorCount(persons));
-    
-    Supplier<Long> countByStream = () -> persons.stream().filter(p -> p.isVendor()).collect(Collectors.counting());
-    invokeMethod("Vendors by Stream", countByStream);
-    Supplier<Long> countByParallelStream = () -> persons.parallelStream().filter(p -> p.isVendor()).collect(Collectors.counting());
-    invokeMethod("Vendors by ParallelStream", countByParallelStream);
+//------------------------------------------------------------------------------------
+
+  private static void simpleStreamsExperiments(List<Person> persons) {
+    countVendorsIn(persons);
+
+    System.out.println("Simple streams experiments: \n");
+    findYoungGirlsIn(persons);
+    averageAgeOfAllGirlsUnder20In(persons);
+    averageAgeOfNoSuchPeople(persons);
+    youngestManIn(persons);
+  }
+
+  private static void countVendorsIn(List<Person> persons) {
+    System.out.println("Comparing counting a filtered stream in different ways:\n");
+    Long numVendors = invokeMethod("Vendors by counting list with external iterator", () -> getVendorCount(persons));
+    System.out.println("numVendors = " + numVendors);
+
+    Supplier<Long> countByStream = () -> (Long) persons.stream().filter(Person::isVendor).count();
+    numVendors = invokeMethod("Vendors by Stream", countByStream);
+    System.out.println("numVendors = " + numVendors);
+
+    Supplier<Long> countByParallelStream = () -> (Long) persons.parallelStream().filter(Person::isVendor).count();
+    numVendors = invokeMethod("Vendors by ParallelStream", countByParallelStream);
+    System.out.println("numVendors = " + numVendors + "\n");
   }
 
   private static long getVendorCount(List<Person> persons) {
@@ -115,6 +136,66 @@ public class StreamsDemo {
       }
     }
     return counter;
+  }
+
+  private static void findYoungGirlsIn(List<Person> persons) {
+    List<Person> youngGirls = persons.stream()
+            .filter(Person::isFemale)
+            .filter(p -> p.getAge() < 20)
+            .collect(Collectors.toList());
+    System.out.println("Number of young girls = " + youngGirls.size());
+
+    System.out.println("Young Girls = " + youngGirls.stream()
+            .map(p -> p.getGivenName() + " " + p.getSurname())
+            .collect(Collectors.toList()));
+  }
+
+  private static void averageAgeOfAllGirlsUnder20In(List<Person> persons) {
+    averageAgeOfAllGirlsUnder(20, persons);
+  }
+
+  // To see what happens if the filtered stream is empty
+  // Collectors.averagingInt(): "If no elements are present, the result is 0."
+  private static void averageAgeOfNoSuchPeople(List<Person> persons) {
+    averageAgeOfAllGirlsUnder(10, persons);  }
+
+  private static void averageAgeOfAllGirlsUnder(int age, List<Person> persons) {
+    Double meanAge = persons.stream()
+            .filter(Person::isFemale)
+            .filter(p -> p.getAge() < age)
+            .collect(Collectors.averagingInt(Person::getAge));
+    if (meanAge > 0) {
+      System.out.println("Mean age of girls under " + age + " = " + meanAge);
+    } else {
+      System.out.println("No girls under " + age);
+    }
+  }
+
+  private static void youngestManIn(List<Person> persons) {
+
+    Optional<Person> youngestMan = persons.stream()
+            .filter(p -> !p.isFemale())
+            .min(Comparator.comparingInt(Person::getAge));
+
+    if (!youngestMan.isPresent()) {
+      System.out.println("There are no men");
+    } else {
+      int youngestAge = youngestMan.get().getAge();
+
+      List<String> youngestMen = persons.stream()
+              .filter(p -> p.getAge() == youngestAge)
+              .filter(p -> !p.isFemale())
+              .map(Person::getName)
+              .collect(Collectors.toList());
+
+      Person youngest = youngestMan.get();
+      System.out.println("Youngest man = " + youngest.getName() + ", age = " + youngest.getAge());
+
+      if (youngestMen.size() > 1) {
+        System.out.println("There are " + youngestMen.size() + " men of the same age (" + youngest.getAge() + "); full list:");
+        System.out.println("youngestMen = " + youngestMen);
+      }
+    }
   }
 
   @SuppressWarnings("UnusedReturnValue")
